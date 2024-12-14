42957b40c368fbd12d4e9a708f780fb9a347df6e
WooLegend <dnwogus4260@naver.com>
2024-12-15 03:00:38 +0900

    feat: 회원가입 폼 구조 마크업

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -7,6 +7,29 @@ export default function SignUp() {
         <Image width={200} height={280} src="/logo-main.svg" alt="Taskify" />
         <span>첫 방문을 확영합니다!</span>
       </header>
+      <form>
+        <fieldset>
+          <label htmlFor="email">이메일</label>
+          <input id="email" type="text" required />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="nickname">닉네임</label>
+          <input id="nickname" type="text" required />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password"></label>
+          <input id="password" type="password" required />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="repeat"></label>
+          <input id="repeat" type="password" required />
+        </fieldset>
+        <fieldset>
+          <input type="checkbox" id="terms-of-use" />
+          <label htmlFor="terms-of-use">이용약관에 동의합니다.</label>
+        </fieldset>
+        <button type="submit">가입하기</button>
+      </form>
     </div>
   );
 }
