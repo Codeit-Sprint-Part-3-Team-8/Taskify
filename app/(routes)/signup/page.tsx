4dfdbdff5f2c33326e59fc28e5d00bb4d318faf5
WooLegend <dnwogus4260@naver.com>
2024-12-15 02:52:19 +0900

    feat: 로고를 감싼 헤더 마크업

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+
 export default function SignUp() {
-  return <div>SignUp Page</div>;
+  return (
+    <div>
+      <header>
+        <Image width={200} height={280} src="/logo-main.svg" alt="Taskify" />
+        <span>첫 방문을 확영합니다!</span>
+      </header>
+    </div>
+  );
 }
