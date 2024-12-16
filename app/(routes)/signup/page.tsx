4b9cbaccf3b4f61d74ce718bfa3c3199a68f48b9
millennium00 <71242138+Woolegend@users.noreply.github.com>
2024-12-16 18:25:14 +0900

    [Feat] 회원가입 페이지 HTML 구조 구현 (#12)
    * feat: 메인 로고 이미지 에셋 추가
    
    * feat: 로고를 감싼 헤더 마크업
    
    * feat: 회원가입 폼 구조 마크업
    
    * feat: 로그인하기 링크 마크업 #11
    
    * feat: 로고 아이콘 메인페지로 이동을 위한 링크 연결
    
    * feat: 비밀번호 및 비밀번호 확인 라벨 텍스트 삽입

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -1,3 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function SignUp() {
-  return <div>SignUp Page</div>;
+  return (
+    <div>
+      <header>
+        <Link href="/">
+          <Image width={200} height={280} src="/logo-main.svg" alt="Taskify" />
+        </Link>
+        <span>첫 방문을 확영합니다!</span>
+      </header>
+      <form>
+        <fieldset>
+          <label htmlFor="email">이메일</label>
+          <input id="email" type="text" required />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="nickname">닉네임</label>
+          <input id="nickname" type="text" required />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">비밀번호</label>
+          <input id="password" type="password" required />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="repeat">비밀번호 확인</label>
+          <input id="repeat" type="password" required />
+        </fieldset>
+        <fieldset>
+          <input type="checkbox" id="terms-of-use" />
+          <label htmlFor="terms-of-use">이용약관에 동의합니다.</label>
+        </fieldset>
+        <button type="submit">가입하기</button>
+      </form>
+      <footer>
+        <span>이미 회원이신가요?&nbsp;</span>
+        <Link href="/login">로그인하기</Link>
+      </footer>
+    </div>
+  );
 }
