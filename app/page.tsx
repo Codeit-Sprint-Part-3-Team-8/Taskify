2d46fd3d1ad2f8070c898194897a57309a486ae0
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-17 00:12:58 +0900

    refactor: 불필요한 console.log 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import Sidebar from './_components/sidebar/SideBar';
-
 export default function Home() {
-  return (
-    <div>
-      <Sidebar />
-      <h1>Hello Next JS!!!</h1>
-    </div>
-  );
+  return <h1>Hello Next JS!!!</h1>;
 }
