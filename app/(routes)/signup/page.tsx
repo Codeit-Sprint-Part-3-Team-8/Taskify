b42087fcf1912c379545be0d9777515917ef3ed5
WooLegend <dnwogus4260@naver.com>
2024-12-16 20:51:48 +0900

    feat: 커스텀 체크박스 구현

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -71,9 +71,17 @@ export default function SignUp() {
             required
           />
         </fieldset>
-        <fieldset>
-          <input type="checkbox" id="terms-of-use" />
-          <label className={`${FIEDL_LABEL_STYLE} ml-2`} htmlFor="terms-of-use">
+        <fieldset className="flex items-center gap-2">
+          <input className="hidden" type="checkbox" id="terms-of-use" />
+          <label className="relative h-5 w-5" htmlFor="terms-of-use">
+            <Image
+              fill
+              src="/images/icon-checkbox-default.png"
+              alt="이용약관"
+            />
+            <div className="absolute z-10 h-full w-full rounded bg-black-171717 opacity-0 hover:opacity-20 active:opacity-40" />
+          </label>
+          <label className={`${FIEDL_LABEL_STYLE}`} htmlFor="terms-of-use">
             이용약관에 동의합니다.
           </label>
         </fieldset>
