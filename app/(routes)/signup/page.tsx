58be1ef6c7dacd14a9dcaf9223cf1709456baeda
WooLegend <dnwogus4260@naver.com>
2024-12-16 21:01:42 +0900

    feat: 불필요한 유저 셀렉터 제거

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const FIELD_STYLE = 'w-full flex flex-col gap-2';
-const FIEDL_LABEL_STYLE = 'text-black-333236';
+const FIEDL_LABEL_STYLE = 'text-black-333236 select-none';
 const FIELD_INPUT_STYLE =
-  'border border-gray-D9D9D9 rounded-lg px-4 py-3.5 placeholder:text-gray-9FA6B2';
+  'rounded-lg border border-gray-D9D9D9 px-4 py-3.5 placeholder:select-none placeholder:text-gray-9FA6B2';
 
 export default function SignUp() {
   return (
     <div className="fixed left-1/2 right-1/2 top-1/2 w-full max-w-xs -translate-x-1/2 -translate-y-1/2 tablet:max-w-lg">
-      <header className="mb-7 flex flex-col items-center justify-center">
+      <header className="mb-7 flex select-none flex-col items-center justify-center">
         <Link href="/">
           <Image
             width={200}
@@ -73,7 +73,10 @@ export default function SignUp() {
         </fieldset>
         <fieldset className="flex items-center gap-2">
           <input className="hidden" type="checkbox" id="terms-of-use" />
-          <label className="relative h-5 w-5" htmlFor="terms-of-use">
+          <label
+            className="relative h-5 w-5 cursor-pointer select-none"
+            htmlFor="terms-of-use"
+          >
             <Image
               fill
               src="/images/icon-checkbox-default.png"
@@ -81,19 +84,22 @@ export default function SignUp() {
             />
             <div className="absolute z-10 h-full w-full rounded bg-black-171717 opacity-0 hover:opacity-20 active:opacity-40" />
           </label>
-          <label className={`${FIEDL_LABEL_STYLE}`} htmlFor="terms-of-use">
+          <label
+            className={`${FIEDL_LABEL_STYLE} cursor-pointer`}
+            htmlFor="terms-of-use"
+          >
             이용약관에 동의합니다.
           </label>
         </fieldset>
         <button
-          className="w-full rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
+          className="w-full select-none rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
           type="submit"
           disabled
         >
           가입하기
         </button>
       </form>
-      <footer>
+      <footer className="select-none">
         <span>이미 회원이신가요?&nbsp;</span>
         <Link className="text-violet-5534DA underline" href="/login">
           로그인하기
