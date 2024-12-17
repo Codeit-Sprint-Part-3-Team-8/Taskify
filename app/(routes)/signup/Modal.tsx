873aba21f89e68d0f3e0456f2f289877f1e6ee4d
WooLegend <dnwogus4260@naver.com>
2024-12-18 06:07:00 +0900

    test: 테스트 모달 스타일 개선

--- app/(routes)/signup/Modal.tsx
+++ app/(routes)/signup/Modal.tsx
@@ -7,7 +7,7 @@ export default function Modal({
 }) {
   return (
     <div className="fixed z-20 flex h-full w-full items-center justify-center bg-black-171717/80">
-      <div className="flex h-48 w-80 flex-col items-center justify-center gap-8 rounded-2xl bg-white px-16 py-10">
+      <div className="flex h-48 min-w-80 flex-col items-center justify-center gap-8 rounded-2xl bg-white px-16 py-10">
         <div className="text-xl font-medium text-black-333236">{text}</div>
         <button
           className="w-full rounded-lg bg-violet-5534DA py-3 font-semibold text-white hover:bg-violet-700 active:bg-violet-800"
