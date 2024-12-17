ad51fc35202e5e1a52b7b5c01f8d9c6ff819deb3
yun yeji <83654790+yunrap@users.noreply.github.com>
2024-12-17 16:55:50 +0900

    랜딩페이지 기본작업 및 favicon 추가 (#22)
    * feat: 메타태그설정 및 메인페이지 마크업 초기작업
    
    * refactor: 파일위치변경 및 이미지태그 close 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import mainImage from '@images/main.png';
+
 export default function Home() {
-  return <h1>Hello Next JS!!!</h1>;
+  return (
+    <>
+      <div className="bg-black-000000">
+        <div className="justify flex flex-col items-center">
+          <Image src={mainImage} width={722} height={422} alt="메인이미지" />
+          <h1 className="">새로운 일정 관리</h1>
+        </div>
+      </div>
+    </>
+  );
 }
