a66b185f72bfb2d6d180c60937dc27b7615b05be
k-jipgeria <sgim43547@gmail.com>
2024-12-17 17:54:21 +0900

    refactor: 공통 버튼을 커스텀할 수 있도록 customStyle을 prop으로 받도록 수정

---
+++ app/_components/Button/Button.tsx
@@ -0,0 +1,28 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  backgroundColor: 'purple' | 'white';
+  children: React.ReactNode;
+  customStyle?: string;
+}
+
+const BUTTON_COMMON_STYLES = 'font-medium text-base rounded-lg';
+
+const BUTTON_VARIANT_STYLES = {
+  purple: 'bg-violet-5534DA text-white',
+  white: 'bg-white text-gray-787486 border border-gray-D9D9D9',
+};
+
+const ModalButton = ({
+  backgroundColor,
+  children,
+  customStyle,
+}: ButtonProps) => {
+  return (
+    <button
+      className={`${BUTTON_VARIANT_STYLES[backgroundColor]} ${BUTTON_COMMON_STYLES} ${customStyle}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ModalButton;
