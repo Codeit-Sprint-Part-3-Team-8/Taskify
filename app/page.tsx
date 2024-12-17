a32963ead0599aeab0d3038ef897ffc379c8a99e
yunrap <dbsdpwl7@gmail.com>
2024-12-17 15:21:02 +0900

    refactor: 파일위치변경 및 이미지태그 close 변경

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,7 @@ export default function Home() {
     <>
       <div className="bg-black-000000">
         <div className="justify flex flex-col items-center">
-          <Image
-            src={mainImage}
-            width={722}
-            height={422}
-            alt="메인이미지"
-          ></Image>
+          <Image src={mainImage} width={722} height={422} alt="메인이미지" />
           <h1 className="">새로운 일정 관리</h1>
         </div>
       </div>
