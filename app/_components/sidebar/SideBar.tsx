4b9bbda2d0ead4226d298b382ce2caf1483baff5
bhoh032019 <162112399+bhoh032019@users.noreply.github.com>
2024-12-17 15:29:40 +0900

    [Feat] 사이드바 api 연결 (#31)
    * feat: 내 대시보드 목록 api 연결
    
    * feat: 내가 만든 대시보드 여부에 따른 이미지 추가
    
    * feat: 대시보드 목록 색깔 추가
    
    * refactor: 불필요한 console.log 제거
    
    * refactor: console.log추가 제거
    
    * refactor: 중첩된 삼항 연산자를 제거

--- app/_components/sidebar/SideBar.tsx
+++ app/_components/sidebar/SideBar.tsx
@@ -1,7 +1,53 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import instance from '@/api/axios';
+import { useEffect, useState } from 'react';
+
+interface DashBoard {
+  id: number;
+  title: string;
+  color: string;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  createdByMe: boolean;
+}
+
+interface DashBoardResponse {
+  dashboards: DashBoard[];
+  totalCount: number;
+  cursorId: number | null;
+}
+
+const ACCESSTOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDk1OCwidGVhbUlkIjoiMTEtOCIsImlhdCI6MTczNDMzMTY1OSwiaXNzIjoic3AtdGFza2lmeSJ9.NUwX8wxDLIx4GWjslfJYQ-jaxA0AsSLSZXcmK9r0sog';
 
 export default function Sidebar() {
+  const [myDashBoards, setMyDashBoards] = useState<DashBoard[]>([]);
+
+  async function getMyDashBoardList() {
+    try {
+      const response = await instance.get<DashBoardResponse>(
+        `11-8/dashboards?navigationMethod=pagination`,
+        {
+          headers: {
+            Authorization: `Bearer ${ACCESSTOKEN}`,
+          },
+        },
+      );
+
+      setMyDashBoards(response.data.dashboards || []);
+    } catch (error) {
+      console.error('내 대시보드를 불러오는데 실패했습니다.', error);
+    }
+  }
+
+  useEffect(() => {
+    getMyDashBoardList();
+  }, []);
+
   return (
     <div>
       <Link href={'/'}>
@@ -21,7 +67,27 @@ export default function Sidebar() {
           alt="Plusbtn"
         />
       </button>
-      <div>대쉬보드 목록</div>
+      <div>
+        {myDashBoards.length > 0 &&
+          myDashBoards.map((dashboard) => (
+            <div key={dashboard.id}>
+              <div
+                className="h-2 w-2 rounded-full"
+                style={{ backgroundColor: dashboard.color }}
+              ></div>
+              {dashboard.title}
+              {dashboard.createdByMe && (
+                <Image
+                  width={17.59}
+                  height={14}
+                  src="/ic-crown.svg"
+                  alt="CrownIcon"
+                />
+              )}
+            </div>
+          ))}
+        {myDashBoards.length === 0 && <div></div>}
+      </div>
     </div>
   );
 }
