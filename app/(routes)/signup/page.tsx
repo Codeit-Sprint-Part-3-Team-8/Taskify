c876513bd72eaa672211bcc8cd6194ff63edc5ca
millennium00 <71242138+Woolegend@users.noreply.github.com>
2024-12-17 15:40:25 +0900

    [Feat] 회원가입 입력값 유효성 검사 구현 (#33)
    * feat: 상태로 입력값 관리 구현
    
    * feat: 입력 필드 컴포넌트로 분리 후 파일 분할
    
    * refactor: 체크박스 필드 컴포넌트로 분리 및 파일 분할
    
    * refactor: 회원가입 header와 footer를 공통 컴포넌트로 분리
    
    * feat: 유효성 검사 클래스 구현
    
    * feat: 유효성 검사 함수 구현

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -1,110 +1,90 @@
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 
-const FIELD_STYLE = 'w-full flex flex-col gap-2';
-const FIEDL_LABEL_STYLE = 'text-black-333236 select-none';
-const FIELD_INPUT_STYLE =
-  'rounded-lg border border-gray-D9D9D9 px-4 py-3.5 placeholder:select-none placeholder:text-gray-9FA6B2';
+import { useCallback, useEffect, useState } from 'react';
+import InputField from './InputField';
+import CheckboxField from './CheckboxField';
+import AuthHeader from './AuthHeader';
+import AuthFooter from './AuthFooter';
+import { DEFAULT_VALIDATIONS, validateSchema } from './validate';
+import { ValuesType } from './signupType';
+
+const DEFAULT_VALUES: ValuesType = {
+  email: '',
+  nickname: '',
+  password: '',
+  repeat: '',
+};
 
 export default function SignUp() {
+  const [values, setValues] = useState(DEFAULT_VALUES);
+  const [isChecked, setIsChecked] = useState(false);
+  const [validations, setValidations] = useState(DEFAULT_VALIDATIONS);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setValues((prev) => {
+      const next = { ...prev, [name]: value };
+      setValidations((prev) => ({ ...prev, ...validateSchema(name, next) }));
+      return next;
+    });
+  };
+
+  const handleChangeCheckbox = () => setIsChecked((prev) => !prev);
+
+  const validateForm = useCallback(() => {
+    const { email, nickname, password, repeat } = validations;
+    const isValid =
+      email.isValid &&
+      nickname.isValid &&
+      password.isValid &&
+      repeat.isValid &&
+      isChecked;
+    setIsFormValid(isValid);
+  }, [validations, isChecked]);
+
+  useEffect(() => {
+    validateForm();
+  }, [validateForm]);
+
   return (
     <div className="fixed left-1/2 right-1/2 top-1/2 w-full max-w-xs -translate-x-1/2 -translate-y-1/2 tablet:max-w-lg">
-      <header className="mb-7 flex select-none flex-col items-center justify-center">
-        <Link href="/">
-          <Image
-            width={200}
-            height={280}
-            src="/images/logo-main.svg"
-            alt="Taskify"
-          />
-        </Link>
-        <span className="text-xl font-medium text-black-333236">
-          첫 방문을 확영합니다!
-        </span>
-      </header>
+      <AuthHeader />
       <form className="mb-6 flex flex-col gap-6">
-        <fieldset className={FIELD_STYLE}>
-          <label className={FIEDL_LABEL_STYLE} htmlFor="email">
-            이메일
-          </label>
-          <input
-            className={FIELD_INPUT_STYLE}
-            id="email"
-            type="text"
-            placeholder="이메일을 입력해주세요"
-            required
-          />
-        </fieldset>
-        <fieldset className={FIELD_STYLE}>
-          <label className={FIEDL_LABEL_STYLE} htmlFor="nickname">
-            닉네임
-          </label>
-          <input
-            className={FIELD_INPUT_STYLE}
-            id="nickname"
-            type="text"
-            placeholder="닉네임을 입력해주세요"
-            required
-          />
-        </fieldset>
-        <fieldset className={FIELD_STYLE}>
-          <label className={FIEDL_LABEL_STYLE} htmlFor="password">
-            비밀번호
-          </label>
-          <input
-            className={FIELD_INPUT_STYLE}
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-            required
-          />
-        </fieldset>
-        <fieldset className={FIELD_STYLE}>
-          <label className={FIEDL_LABEL_STYLE} htmlFor="repeat">
-            비밀번호 확인
-          </label>
-          <input
-            className={FIELD_INPUT_STYLE}
-            id="repeat"
-            type="password"
-            placeholder="비밀번호를 한 번 더 입력해주세요"
-            required
-          />
-        </fieldset>
-        <fieldset className="flex items-center gap-2">
-          <input className="hidden" type="checkbox" id="terms-of-use" />
-          <label
-            className="relative h-5 w-5 cursor-pointer select-none"
-            htmlFor="terms-of-use"
-          >
-            <Image
-              fill
-              src="/images/icon-checkbox-default.png"
-              alt="이용약관"
-            />
-            <div className="absolute z-10 h-full w-full rounded bg-black-171717 opacity-0 hover:opacity-20 active:opacity-40" />
-          </label>
-          <label
-            className={`${FIEDL_LABEL_STYLE} cursor-pointer`}
-            htmlFor="terms-of-use"
-          >
-            이용약관에 동의합니다.
-          </label>
-        </fieldset>
+        <InputField
+          name="email"
+          value={values.email}
+          validation={validations.email}
+          onChange={handleChangeValue}
+        />
+        <InputField
+          name="nickname"
+          value={values.nickname}
+          validation={validations.nickname}
+          onChange={handleChangeValue}
+        />
+        <InputField
+          name="password"
+          value={values.password}
+          validation={validations.password}
+          onChange={handleChangeValue}
+        />
+        <InputField
+          name="repeat"
+          value={values.repeat}
+          validation={validations.repeat}
+          onChange={handleChangeValue}
+        />
+        <CheckboxField isChecked={isChecked} onChange={handleChangeCheckbox} />
         <button
           className="w-full select-none rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
           type="submit"
-          disabled
+          disabled={!isFormValid}
         >
           가입하기
         </button>
       </form>
-      <footer className="select-none text-center">
-        <span>이미 회원이신가요?&nbsp;</span>
-        <Link className="text-violet-5534DA underline" href="/login">
-          로그인하기
-        </Link>
-      </footer>
+      <AuthFooter to="signin" />
     </div>
   );
 }
---
+++ app/(routes)/signup/validate.ts
@@ -0,0 +1,59 @@
+import Validator from '@/_lib/validator';
+import { ValidationType, ValuesType } from './signupType';
+
+const DEFAULT_VALIDATIONS: ValidationType = {
+  email: { isValid: false, message: '' },
+  nickname: { isValid: false, message: '' },
+  password: { isValid: false, message: '' },
+  repeat: { isValid: false, message: '' },
+};
+
+function validateSchema(name: string, values: ValuesType) {
+  switch (name) {
+    case 'email':
+      const emailSchema = new Validator(values.email)
+        .required('이메일을 입력해주세요')
+        .isEmail('이메일 형식이 옳지 않습니다');
+      const email = {
+        isValid: emailSchema.validate(),
+        message: emailSchema.validate() ? '' : emailSchema.getErrors()[0],
+      };
+      return { email };
+    case 'nickname':
+      const nicknameSchema = new Validator(values.nickname)
+        .required('닉네임을 입력해주세요')
+        .minLength(2, '닉네임을 2자 이상 입력해주세요')
+        .maxLength(10, '이메일을 10자 이하로 입력해주세요');
+      const nickname = {
+        isValid: nicknameSchema.validate(),
+        message: nicknameSchema.validate() ? '' : nicknameSchema.getErrors()[0],
+      };
+      return { nickname };
+    case 'password':
+    case 'repeat':
+      const passwordSchema = new Validator(values.password)
+        .required('비밀번호를 입력해주세요')
+        .minLength(8, '비밀번호를 8자 이상 입력해주세요');
+
+      const password = {
+        isValid: passwordSchema.validate(),
+        message: passwordSchema.validate() ? '' : passwordSchema.getErrors()[0],
+      };
+
+      const repeat = { isValid: false, message: '' };
+      if (values.repeat.trim().length > 0) {
+        if (values.repeat.trim() === values.password.trim()) {
+          repeat.isValid = true;
+        } else {
+          repeat.message = '비밀번호가 일치하지 않습니다.';
+        }
+      }
+      return {
+        password,
+        repeat,
+      };
+  }
+}
+
+export { validateSchema, DEFAULT_VALIDATIONS };
+export type { ValidationType };
