5d9b48aa52be5c46bb2b307acf1df6949bb5022b
WooLegend <dnwogus4260@naver.com>
2024-12-18 06:07:38 +0900

    feat: useAsync 커스텀 훅 에러메세지 및 상태코드 반환 구현

--- app/_hooks/useAsync.ts
+++ app/_hooks/useAsync.ts
@@ -1,11 +1,14 @@
-import { useCallback, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function useAsync<T, R>(
   asyncFunction: (params: T) => Promise<R | null>,
 ) {
   const [data, setData] = useState<R | null>(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [errorStatus, setErrorStatus] = useState<number | null>(null);
 
   const excute = useCallback(
     async (params: T) => {
@@ -14,8 +17,8 @@ export default function useAsync<T, R>(
         setError(null);
         const response = await asyncFunction(params);
         setData(response);
-      } catch (e) {
-        setError(e as Error);
+      } catch (err) {
+        setError(err as AxiosError);
       } finally {
         setLoading(false);
       }
@@ -23,5 +26,18 @@ export default function useAsync<T, R>(
     [asyncFunction],
   );
 
-  return { data, excute, loading, error };
+  useEffect(() => {
+    if (axios.isAxiosError(error)) {
+      const { message } = error.response?.data as {
+        message: string;
+      };
+      setErrorMessage(message || 'Error Occured');
+      setErrorStatus(error.status || null);
+    } else {
+      setErrorMessage(null);
+      setErrorStatus(null);
+    }
+  }, [error]);
+
+  return { data, excute, loading, error, errorMessage, errorStatus };
 }
