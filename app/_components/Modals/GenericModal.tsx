dba29d96f177f50c71dde1653531d3d4c351f611
k-jipgeria <sgim43547@gmail.com>
2024-12-17 13:48:46 +0900

    refactor: 모달 컨텐츠는 props로 받고, 모달의 틀 역할에만 초점을 맞춤.
    - 모든 모달에서 모달 외 공간 클릭 시 모달 Close
    - 모든 모달에서 백그라운드 어둡게 설정
    - title, mainContent, footerContenet를 모두 props로 받으므로써 유연성 증가

--- app/_components/Modals/GenericModal.tsx
+++ app/_components/Modals/GenericModal.tsx
@@ -1,31 +1,38 @@
-import ModalButton from '@/_components/Modals/ModalButton'
-
 interface GenericModalProps {
-  children: React.ReactNode
-  leftButtonText: string
-  rightButtonText: string
-  onClose: () => void
-  onSubmit?: () => void
+  title?: string;
+  onClose?: () => void;
+  mainContent: React.ReactNode;
+  footerContent: React.ReactNode;
 }
 
-const GenericModal = ({ children, leftButtonText, rightButtonText, onClose, onSubmit }: GenericModalProps) => {
+const GenericModal = ({
+  title,
+  onClose,
+  mainContent,
+  footerContent,
+}: GenericModalProps) => {
+  const handleClose = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      onClose?.();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <div className="bg-white rounded-lg p-4">
-        {children}
-        <footer>
-          <div className="flex items-center justify-between tablet:gap-2 mobile:gap-[7px]">
-            <ModalButton backgroundColor="white" onClick={onClose}>
-              {leftButtonText}
-            </ModalButton>
-            <ModalButton backgroundColor="purple" onClick={onSubmit}>
-              {rightButtonText}
-            </ModalButton>
-          </div>
-        </footer>
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black-000000/30"
+      onClick={handleClose}
+    >
+      <div className="rounded-lg bg-white p-4">
+        {title && (
+          <header>
+            <div>{title}</div>
+          </header>
+        )}
+        <main>{mainContent}</main>
+        <footer>{footerContent}</footer>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GenericModal
+export default GenericModal;
