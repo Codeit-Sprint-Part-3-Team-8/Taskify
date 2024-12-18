55f8c7e730ef7e1e156acc1907880e2701de7f75
WooLegend <dnwogus4260@naver.com>
2024-12-19 06:10:39 +0900

    feat: 로그인 페이지 useAuth 적용

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -6,9 +6,7 @@ import InputField from '@/_components/Auth/InputField';
 import { useCallback, useEffect, useState } from 'react';
 import { ValuesType } from './loginType';
 import { DEFAULT_VALIDATIONS, validateSchema } from './validate';
-import useAsync from '@/_hooks/useAsync';
-import { loginUser } from '@/api/auth';
-import Modal from '@/_components/Auth/Modal';
+import { useAuth } from '@/context/AuthContext';
 
 const DEFAULT_VALUES: ValuesType = {
   email: '',
@@ -19,16 +17,7 @@ export default function LoginPage() {
   const [values, setValues] = useState(DEFAULT_VALUES);
   const [validations, setValidations] = useState(DEFAULT_VALIDATIONS);
   const [isFormValid, setIsFormValid] = useState(false);
-  const [showError, setShowError] = useState(false);
-  const {
-    data: loginData,
-    excute: loginUserAsync,
-    loading,
-    error,
-    errorMessage,
-  } = useAsync(loginUser);
-
-  const handleClickClose = () => setShowError(false);
+  const { login } = useAuth();
 
   const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -51,16 +40,8 @@ export default function LoginPage() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!isFormValid) return;
-    await loginUserAsync(values);
+    login(values);
   };
-
-  useEffect(() => {
-    if (error) {
-      setShowError(true);
-    } else {
-      setShowError(false);
-    }
-  }, [error]);
 
   useEffect(() => {
     validateForm();
@@ -68,12 +49,6 @@ export default function LoginPage() {
 
   return (
     <>
-      {!!loginData && (
-        <Modal text="로그인이 완료되었습니다!" onClick={handleClickClose} />
-      )}
-      {showError && (
-        <Modal text={errorMessage as string} onClick={handleClickClose} />
-      )}
       <div className="fixed left-1/2 right-1/2 top-1/2 w-full max-w-xs -translate-x-1/2 -translate-y-1/2 tablet:max-w-lg">
         <AuthHeader text="오늘도 만나서 반가워요!" />
         <form className="mb-6 flex flex-col gap-6" onSubmit={handleSubmit}>
@@ -92,7 +67,7 @@ export default function LoginPage() {
           <button
             className="w-full select-none rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
             type="submit"
-            disabled={!isFormValid || loading}
+            disabled={!isFormValid}
           >
             로그인
           </button>
