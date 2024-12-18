d02be05dc97076c028ebd237f2423d05dfd049d6
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-19 01:49:46 +0900

    feat: 메인 랜딩페이지 navbar UI 디자인

--- app/_components/Navbar/HomeNavBar.tsx
+++ app/_components/Navbar/HomeNavBar.tsx
@@ -3,27 +3,34 @@ import Image from 'next/image';
 
 export default function HomeNavBar() {
   return (
-    <div className="bg-black-000000">
-      <Image
-        width={35}
-        height={35}
-        src="/images/logo/logo-image-white.svg"
-        alt="logoImage"
-      />
-      <Image
-        width={80}
-        height={22}
-        src="/images/logo/logo-text-white.svg"
-        alt="logoText"
-      />
-      <ul>
+    <nav className="flex h-[3.75rem] w-full justify-between gap-2.5 bg-black-000000 px-6 py-4 tablet:h-[4.375rem] tablet:px-10 pc:px-20">
+      <Link href="/" className="flex">
+        <Image
+          width={35}
+          height={35}
+          src="/images/logo/logo-image-white.svg"
+          alt="logoImage"
+        />
+        <Image
+          className="hidden tablet:block"
+          width={80}
+          height={22}
+          src="/images/logo/logo-text-white.svg"
+          alt="logoText"
+        />
+      </Link>
+      <ul className="flex items-center justify-center gap-6 tablet:gap-9">
         <li>
-          <Link href="/login">로그인</Link>
+          <Link className="text-sm text-white tablet:text-base" href="/login">
+            로그인
+          </Link>
         </li>
         <li>
-          <Link href="/signup">회원가입</Link>
+          <Link className="text-sm text-white tablet:text-base" href="/signup">
+            회원가입
+          </Link>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
