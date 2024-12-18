2bfcd74cc17af20cbbcb18dcf7fa7d7b734f8fdf
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-18 15:50:07 +0900

    refactor: 불필요한 임시 sidebar 컴포넌트 제거

--- app/(routes)/mydashboard/page.tsx
+++ app/(routes)/mydashboard/page.tsx
@@ -1,10 +1,8 @@
-import Sidebar from '@/_components/sidebar/SideBar';
 import DashboardList from './DashboardList';
 
 export default function myDashBoardPage() {
   return (
     <div>
-      <Sidebar />
       <DashboardList />
     </div>
   );
