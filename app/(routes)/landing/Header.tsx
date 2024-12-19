1bd93bfa8d191897b14cb165d6ddc684559b66ae
WooLegend <dnwogus4260@naver.com>
2024-12-19 19:51:10 +0900

    merge: dev 브랜치 병합 후 충돌 해결

---
+++ app/(routes)/landing/Header.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import logo from '@images/icon/logo.svg';
+import taskify from '@images/contents/taskify.png';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="px-6 py-4 tablet:px-10 pc:px-20">
+      <div className="flex max-w-[1760px] justify-between">
+        <Link href="/" className="flex items-center">
+          <Image src={logo} width={35} height={35} alt="Taskify 로고" />
+          <Image src={taskify} width={80} height={22} alt="Taskify 로고" />
+        </Link>
+        <div className="flex items-center gap-9">
+          <Link href="/login">
+            <div className="cursor-pointer text-lg font-normal text-white">
+              로그인
+            </div>
+          </Link>
+          <Link href="/login">
+            <div className="cursor-pointer text-lg font-normal text-white">
+              회원가입
+            </div>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import Image from 'next/image';
-import mainImage from '@images/contents/main.png';
+import LandingPage from './(routes)/landing/page';
 
 export default function Home() {
-  return (
-    <>
-      <div className="bg-black-000000">
-        <div className="justify flex flex-col items-center">
-          <Image src={mainImage} width={722} height={422} alt="메인이미지" />
-          <h1 className="">새로운 일정 관리</h1>
-        </div>
-      </div>
-    </>
-  );
+  return <LandingPage />;
 }
