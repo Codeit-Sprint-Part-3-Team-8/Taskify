76b6b0c246ba23269375d32f5883542cc338da74
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-19 15:14:31 +0900

    feat: mydashboard페이지 네비게이션 바 UI 디자인

--- app/_components/Navbar/MyDashboardNavBar.tsx
+++ app/_components/Navbar/MyDashboardNavBar.tsx
@@ -2,16 +2,20 @@ import Image from 'next/image';
 
 export default function MyDashboardNavBar() {
   return (
-    <div className="flex w-full justify-between pl-80">
-      <div>내 대시보드</div>
-      <div className="flex items-center">
+    <div className="flex h-[3.75rem] w-full items-center justify-between border-b border-gray-D9D9D9 pl-[5.25rem] pr-2 tablet:h-[4.375rem] tablet:pl-[12.5rem] tablet:pr-8 pc:pl-80 pc:pr-20">
+      <div className="text-lg text-black-333236 tablet:text-xl">
+        내 대시보드
+      </div>
+      <div className="flex items-center tablet:gap-3">
         <Image
           width={38}
           height={38}
           src="/images/contents/default-profile.svg"
           alt="profile"
         />
-        <div>사용자</div>
+        <div className="hidden text-lg text-black-333236 tablet:block">
+          사용자
+        </div>
       </div>
     </div>
   );
