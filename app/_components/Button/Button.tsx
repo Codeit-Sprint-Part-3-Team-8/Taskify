298b975fb1086456f08e8734839f5fe7eafadf6e
k-jipgeria <sgim43547@gmail.com>
2024-12-20 23:52:59 +0900

    refactor: disabled 상태에서의 스타일 추가

--- app/_components/Button/Button.tsx
+++ app/_components/Button/Button.tsx
@@ -2,23 +2,38 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   backgroundColor: 'purple' | 'white';
   children: React.ReactNode;
   customStyle?: string;
+  disabled?: boolean;
 }
 
-const BUTTON_COMMON_STYLES = 'font-medium text-base rounded-lg';
-
-const BUTTON_VARIANT_STYLES = {
-  purple: 'bg-violet-5534DA text-white',
-  white: 'bg-white text-gray-787486 border border-gray-D9D9D9',
-};
+const BUTTON_STYLES = {
+  base: 'rounded-lg text-base font-medium transition-opacity',
+  variant: {
+    purple: 'bg-violet-5534DA text-white',
+    white: 'bg-white text-gray-787486 border border-gray-D9D9D9',
+  },
+  state: {
+    enabled: 'hover:opacity-80',
+    disabled: 'opacity-50 cursor-not-allowed'
+  }
+} as const;
 
 const Button = ({
   backgroundColor,
   children,
   customStyle,
+  disabled,
+  ...props
 }: ButtonProps) => {
   return (
     <button
-      className={`${BUTTON_VARIANT_STYLES[backgroundColor]} ${BUTTON_COMMON_STYLES} ${customStyle}`}
+      className={`
+        ${BUTTON_STYLES.variant[backgroundColor]}
+        ${BUTTON_STYLES.base}
+        ${disabled ? BUTTON_STYLES.state.disabled : BUTTON_STYLES.state.enabled}
+        ${customStyle}
+      `}
+      disabled={disabled}
+      {...props}
     >
       {children}
     </button>
