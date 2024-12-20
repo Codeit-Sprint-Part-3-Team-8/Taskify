05b87bb0abd9ce1b1575798b151a1ee4b2b72ee8
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-20 18:12:29 +0900

    feat: 로그아웃시 토큰 제거 후 메인 페이지로 이동

--- app/_components/Navbar/DropdownContent.tsx
+++ app/_components/Navbar/DropdownContent.tsx
@@ -1,7 +1,14 @@
+import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
 
 export default function DropdownContet() {
   const router = useRouter();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    router.push('/');
+  };
 
   return (
     <>
@@ -22,7 +29,7 @@ export default function DropdownContet() {
       <button
         className="block w-full px-4 py-2 text-center text-sm text-black-333236 hover:bg-gray-100"
         type="button"
-        onClick={() => alert('로그아웃 클릭됨')}
+        onClick={handleLogout}
       >
         로그아웃
       </button>
