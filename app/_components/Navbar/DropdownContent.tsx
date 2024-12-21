99159a2ae5f848eca4cadeac8270432262019551
bhoh032019 <162112399+bhoh032019@users.noreply.github.com>
2024-12-21 16:11:06 +0900

    [Feat] 대시보드 페이지 네비게이션 바 구현하기 (#68)
    * feat: 폰트전역 추가 작업 (#57)
    
    * dashboard 네비게이션 바 구조 구현
    
    * feat: dashboard 네비게이션 바 UI 디자인
    
    * feat: dashboard 네비게이션바 api 연결
    
    * feat: 관리 버튼 클릭시 /dashboard/{dashboardid}/edit 페이지로 이동
    
    * feat: dropdown 기능 추가
    
    * feat: 로그아웃시 토큰 제거 후 메인 페이지로 이동
    
    * refactor: member type 수정
    
    * refactor: screensize 상수로 정의
    
    * refactor: 단순 페이지 이동의 경우  Link를 이용하도록 수정
    
    ---------
    
    Co-authored-by: yun yeji <83654790+yunrap@users.noreply.github.com>

---
+++ app/_components/Navbar/DropdownContent.tsx
@@ -0,0 +1,37 @@
+import { useAuth } from '@/context/AuthContext';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+export default function DropdownContent() {
+  const router = useRouter();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    router.push('/');
+  };
+
+  return (
+    <>
+      <Link
+        className="block w-full border-b px-4 py-2 text-center text-sm text-black-333236 hover:bg-gray-100"
+        href="/mypage"
+      >
+        내 정보
+      </Link>
+      <Link
+        className="block w-full border-b px-4 py-2 text-center text-sm text-black-333236 hover:bg-gray-100"
+        href="/mydashboard"
+      >
+        내 대시보드
+      </Link>
+      <button
+        className="block w-full px-4 py-2 text-center text-sm text-black-333236 hover:bg-gray-100"
+        type="button"
+        onClick={handleLogout}
+      >
+        로그아웃
+      </button>
+    </>
+  );
+}
