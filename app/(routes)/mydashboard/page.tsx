cd8ff0970a02cd08840bbdca948f0d8ce7e1fbbc
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-21 15:49:36 +0900

    refactor: mydashboard 네비게이션 바 드롭다운 메뉴 컴포넌트를 이용하도록 변경

--- app/(routes)/mydashboard/page.tsx
+++ app/(routes)/mydashboard/page.tsx
@@ -1,11 +1,11 @@
 import Sidebar from '@/_components/Sidebar/SideBar';
 import DashboardList from './DashboardList';
-import MyDashboardNavBar from '@/_components/Navbar/MyDashboardNavBar';
+import NavBar from '@/_components/Navbar/NavBar';
 
 export default function myDashBoardPage() {
   return (
     <div>
-      <MyDashboardNavBar />
+      <NavBar type="mydashboard" />
       <Sidebar />
       <DashboardList />
     </div>
