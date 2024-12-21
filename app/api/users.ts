ef2a5f850ffc87bfabedffaced4ca0d6075cfe1d
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-21 16:51:11 +0900

    refactor: 불필요한 accessToken 상수 제거

--- app/api/users.ts
+++ app/api/users.ts
@@ -6,9 +6,6 @@ interface CreateUserParams {
   password: string;
 }
 
-const ACCESSTOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDk1OCwidGVhbUlkIjoiMTEtOCIsImlhdCI6MTczNDMzMTY1OSwiaXNzIjoic3AtdGFza2lmeSJ9.NUwX8wxDLIx4GWjslfJYQ-jaxA0AsSLSZXcmK9r0sog';
-
 async function createUser({ email, nickname, password }: CreateUserParams) {
   const response = await axios.post('/users', { email, nickname, password });
   return response.data;
