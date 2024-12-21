a93bf50b7dc5571f11983f886ba38f79f347a43e
yun yeji <83654790+yunrap@users.noreply.github.com>
2024-12-21 18:08:00 +0900

    feat: 대시보드수정 상단카드작업 (#65)
    * feat: 대시보드페이지 상단카드부분 수정기능 및 퍼블구현
    
    * refactor: api명 컨벤션규칙적용 및 상수표현

---
+++ app/(routes)/dashboard/[dashboardId]/edit/ColorSelectBox.tsx
@@ -0,0 +1,56 @@
+'use client';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import iconCheck from '@images/icon/icon_check.svg';
+const COLORS = [
+  'bg-green-7AC555',
+  'bg-purple-760DDE',
+  'bg-orange-FFA500',
+  'bg-blue-76A5EA',
+  'bg-pink-E876EA',
+];
+interface ColorSelectBoxProps {
+  onChangeColor: (color: string) => void;
+  usedColor: string;
+}
+
+export default function ColorSelectBox({
+  onChangeColor,
+  usedColor,
+}: ColorSelectBoxProps) {
+  const [selectedColor, setSelectedColor] = useState<string>(usedColor);
+
+  const handleColorChange = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    const clickedColor = e.currentTarget.classList[0].split('-')[2];
+    const color = `#${clickedColor}`;
+    setSelectedColor(color);
+    onChangeColor(color);
+  };
+
+  return (
+    <div className="flex gap-2">
+      {COLORS.map((data, index) => {
+        const clickedColor = `#${data.split('-')[2]}`;
+        return (
+          <div
+            onClick={handleColorChange}
+            className={`${data} relative h-[1.8rem] w-[1.8rem] gap-2 rounded-2xl`}
+            key={index}
+          >
+            {selectedColor === clickedColor && (
+              <Image
+                src={iconCheck}
+                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+                width={28}
+                height={28}
+                alt="체크"
+              ></Image>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ app/(routes)/dashboard/[dashboardId]/edit/ModifyBox.tsx
@@ -0,0 +1,79 @@
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import ColorSelectBox from './ColorSelectBox';
+import Button from '@/_components/Button/Button';
+import useAsync from '@/_hooks/useAsync';
+import {
+  updateDashboardId,
+  UpdateDashboardIdParams,
+  UpdateDashboardIdReturn,
+} from '@/api/dashboard';
+
+interface ModifyBoxProps {
+  modifyData: UpdateDashboardIdReturn;
+}
+
+export default function ModifyBox({ modifyData }: ModifyBoxProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [colorData, setColorData] = useState(modifyData.color);
+  const [dashboardData, setDashboardData] = useState(modifyData);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const { data: asyncData, excute: updateDashBoardAsync } = useAsync(
+    (params: UpdateDashboardIdParams) =>
+      updateDashboardId(modifyData.id, params),
+  );
+
+  const { title, color } = dashboardData;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const param = { title: inputRef.current!.value, color: colorData };
+    await updateDashBoardAsync(param);
+  };
+
+  const handleInputChange = () => {
+    if (inputRef.current?.value.trim()) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  };
+
+  useEffect(() => {
+    if (asyncData) {
+      // asyncData가 업데이트되면 dashboardData 상태를 즉시 갱신!!
+      setDashboardData(asyncData);
+    }
+  }, [asyncData]);
+
+  return (
+    <div className="mx-3 bg-white px-4 py-5 tablet:w-[34rem] tablet:py-8 pc:w-[38rem]">
+      {/* 박스 */}
+      <form onSubmit={handleSubmit}>
+        <div className="pb-10">
+          <div className="pb-6 text-xl font-bold">{title}</div>
+          <div className="flex flex-col gap-2 pb-4">
+            <div className="text-lg font-medium">대시보드 이름</div>
+            <input
+              onChange={handleInputChange}
+              ref={inputRef}
+              className="border-D9D9D9 rounded-lg border border-solid border-gray-D9D9D9 p-4"
+            ></input>
+          </div>
+          <ColorSelectBox
+            onChangeColor={(color) => setColorData(color)}
+            usedColor={color}
+          />
+        </div>
+        <Button
+          backgroundColor="purple"
+          customStyle="w-full py-3"
+          type="submit"
+          disabled={isButtonDisabled}
+        >
+          변경
+        </Button>
+      </form>
+    </div>
+  );
+}
