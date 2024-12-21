b0c90d9a028774beb90062d600b18ca9c5acc33f
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-21 13:04:05 +0900

    feat: 객체 맵핑을 이용한 동적 navbar 렌더링

--- app/_components/Navbar/NavbaBar.tsx
+++ app/_components/Navbar/NavbaBar.tsx
@@ -1,3 +1,18 @@
-export default function NavBar() {
-  return <div>hi</div>;
+import MyDashboardNavBar from './MyDashboardNavBar';
+import DashboardNavBar from './DashboardNavBar';
+import React from 'react';
+
+interface NavBarComponents {
+  dashboard: React.ReactNode;
+  mydashboard: React.ReactNode;
+}
+
+const NAVBAR_COMPONENTS: NavBarComponents = {
+  dashboard: <DashboardNavBar />,
+  mydashboard: <MyDashboardNavBar />,
+};
+
+export default function NavBar({ type }: { type: keyof NavBarComponents }) {
+  const NavBarComponent = NAVBAR_COMPONENTS[type] || <div>Default NavBar</div>;
+  return <>{NavBarComponent}</>;
 }
