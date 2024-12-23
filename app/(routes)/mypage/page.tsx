3e6084a7120a3754dfbf62b5d1c9e97de9219216
WooLegend <dnwogus4260@naver.com>
2024-12-23 17:10:43 +0900

    feat: 계전 관리 페이지 구조 구현

--- app/(routes)/mypage/page.tsx
+++ app/(routes)/mypage/page.tsx
@@ -1,3 +1,23 @@
+'use client';
+
+import { useAuth } from '@/context/AuthContext';
+import Link from 'next/link';
+import ProfileForm from './ProfileForm';
+import PasswordForm from './PasswordForm';
+
 export default function MyPage() {
-  return <div>마이페이지</div>;
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <Link href="/mydashboard">돌아가기</Link>
+
+      <ProfileForm
+        email={user?.email as string}
+        nickname={user?.nickname as string}
+      />
+
+      <PasswordForm />
+    </div>
+  );
 }
