4c0702571f1a6147d8e56790df8b6093a840fc85
WooLegend <dnwogus4260@naver.com>
2024-12-23 19:25:06 +0900

    feat: 사이드바 네브바 적용

--- app/(routes)/mypage/page.tsx
+++ app/(routes)/mypage/page.tsx
@@ -5,28 +5,34 @@ import Link from 'next/link';
 import ProfileForm from './ProfileForm';
 import PasswordForm from './PasswordForm';
 import Image from 'next/image';
+import MyDashboardNavBar from '@/_components/Navbar/MyDashboardNavBar';
+import SideBar from '@/_components/Sidebar/SideBar';
 
 export default function MyPage() {
   const { user } = useAuth();
 
   return (
-    <div className="bg-gray-F5F5F5">
-      <div className="flex flex-col gap-6 p-6">
-        <Link
-          href="/mydashboard"
-          className="flex w-fit items-center justify-center gap-2"
-        >
-          <div className="relative h-4 w-4 tablet:h-6 tablet:w-6">
-            <Image fill src="/images/icon/icon_arrow.svg" alt="돌아가기" />
-          </div>
-          <span className="text-sm tablet:text-lg">돌아가기</span>
-        </Link>
-        <ProfileForm
-          email={user?.email as string}
-          nickname={user?.nickname as string}
-        />
-        <PasswordForm />
+    <>
+      <SideBar />
+      <MyDashboardNavBar />
+      <div className="bg-gray-F5F5F5 ml-16 tablet:ml-40">
+        <div className="flex flex-col gap-6 p-6">
+          <Link
+            href="/mydashboard"
+            className="flex w-fit items-center justify-center gap-2"
+          >
+            <div className="relative h-4 w-4 tablet:h-6 tablet:w-6">
+              <Image fill src="/images/icon/icon_arrow.svg" alt="돌아가기" />
+            </div>
+            <span className="text-sm tablet:text-lg">돌아가기</span>
+          </Link>
+          <ProfileForm
+            email={user?.email as string}
+            nickname={user?.nickname as string}
+          />
+          <PasswordForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
