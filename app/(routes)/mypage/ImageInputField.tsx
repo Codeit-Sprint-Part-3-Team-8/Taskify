1ee1ad52a496e465ea13e05f07cd6dcb3d26e557
millennium00 <71242138+Woolegend@users.noreply.github.com>
2024-12-25 02:16:45 +0900

    [Feat] 프로필 이미지 업로드 및 미리보기 기능 구현 (#112)
    * fix: UpdateUserParams의 profileImageUrl 타입 수정
    
    * feat: 프로필 변경 폼 이메일 readonly로 설정
    
    * fix: 이미지 업로드 api 헤더 및 관련 타입 수정
    
    * chore: next/image 경로 설정
    
    * feat: 이미지 업로드 구현

--- app/(routes)/mypage/ImageInputField.tsx
+++ app/(routes)/mypage/ImageInputField.tsx
@@ -1,21 +1,54 @@
 import Image from 'next/image';
 
-export default function ImageInputField() {
+interface ImageInputFieldProps {
+  initProfileImageUrl: string | null;
+  profileImageUrl: string | null;
+  onChange: (value: FormData | null) => void;
+}
+
+export default function ImageInputField({
+  initProfileImageUrl,
+  profileImageUrl,
+  onChange,
+}: ImageInputFieldProps) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files: FileList | null = event.target.files;
+    if (files && files[0]) {
+      const formData = new FormData();
+      formData.append('image', files[0], files[0].name);
+      onChange(formData);
+    }
+  };
+
   return (
     <fieldset>
-      <input className="hidden" id="profile" type="file" />
+      <input
+        className="hidden"
+        id="profile"
+        type="file"
+        onChange={handleChange}
+      />
       <label
         htmlFor="profile"
-        className="bg-gray-F5F5F5 flex h-24 w-24 items-center justify-center rounded-[0.4rem] tablet:h-[11.5rem] tablet:w-[11.5rem]"
+        className="relative flex h-40 w-40 items-center justify-center rounded-[0.4rem] bg-gray-F5F5F5 tablet:h-[11.5rem] tablet:w-[11.5rem]"
       >
-        <div className="relative h-5 w-5 tablet:h-8 tablet:w-8">
+        {profileImageUrl || initProfileImageUrl ? (
           <Image
             fill
             sizes="100%, 100%, 100%, 100%"
-            src="/images/icon/icon-plus-purple.png"
-            alt="프로밀 이미지 변경"
+            src={profileImageUrl || (initProfileImageUrl as string)}
+            alt="프로필 이미지"
           />
-        </div>
+        ) : (
+          <div className="relative h-5 w-5 tablet:h-8 tablet:w-8">
+            <Image
+              fill
+              sizes="100%, 100%, 100%, 100%"
+              src="/images/icon/icon-plus-purple.png"
+              alt="프로밀 이미지 변경"
+            />
+          </div>
+        )}
       </label>
     </fieldset>
   );
