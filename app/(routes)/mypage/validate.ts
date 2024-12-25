4beac2397f6025f41dedf679e5618afa14f42dd1
WooLegend <dnwogus4260@naver.com>
2024-12-25 21:58:26 +0900

    feat: 유효성 검사 함수 구현

---
+++ app/(routes)/mypage/validate.ts
@@ -0,0 +1,40 @@
+import Validator from '@/_lib/validator';
+import { ValidationsType, ValidationType } from './types';
+
+const DEFAULT_PROFILE_VALIDATIONS: ValidationsType = {
+  nickname: { isValid: false, message: '' },
+  profileImageUrl: { isValid: false, message: '' },
+};
+
+const validate: {
+  [key: string]: (value: unknown) => ValidationType;
+} = {
+  nickname: (value: unknown) => {
+    if (typeof value === 'string') {
+      const schema = new Validator(value as string)
+        .required('닉네임을 입력해주세요')
+        .minLength(2, '닉네임을 2자 이상 입력해주세요')
+        .maxLength(10, '닉네임을 10자 이하로 입력해주세요');
+      return {
+        isValid: schema.validate(),
+        message: schema.validate() ? '' : schema.getErrors()[0],
+      };
+    }
+    return { isValid: false, message: '잘못된 형식의 입력입니다.' };
+  },
+  profileImageUrl: (value: unknown) => {
+    if (value === null) return { isValid: true, message: '' };
+
+    if (typeof value === 'string') {
+      const schema = new Validator(value).isUrl('잘못된 Url 형식입니다.');
+      return {
+        isValid: schema.validate(),
+        message: schema.validate() ? '' : schema.getErrors()[0],
+      };
+    }
+
+    return { isValid: false, message: '잘못된 형식의 입력입니다.' };
+  },
+};
+
+export { DEFAULT_PROFILE_VALIDATIONS, validate };
