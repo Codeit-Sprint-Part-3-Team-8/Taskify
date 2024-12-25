de6727ab4ddc73433f974d5272eba7c5a7659e31
WooLegend <dnwogus4260@naver.com>
2024-12-25 21:59:18 +0900

    feat: 프로필 변경 폼 유효성 검사 구현

--- app/(routes)/mypage/ProfileForm.tsx
+++ app/(routes)/mypage/ProfileForm.tsx
@@ -3,14 +3,10 @@ import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import ImageInputField from './ImageInputField';
 import useAsync from '@/_hooks/useAsync';
 import { createProfileImage, updateUser } from '@/api/users.api';
+import { ProfileFormProps } from './types';
+import { DEFAULT_PROFILE_VALIDATIONS, validate } from './validate';
 
-interface ProfileFormProps {
-  email: string;
-  nickname: string;
-  profileImageUrl: string | null;
-}
-
-const INIT_VALUES: ProfileFormProps = {
+const DEFAULT_VALUES: ProfileFormProps = {
   email: '',
   nickname: '',
   profileImageUrl: null,
@@ -21,31 +17,59 @@ export default function ProfileForm({
   nickname,
   profileImageUrl,
 }: ProfileFormProps) {
-  const [values, setValues] = useState(INIT_VALUES);
-  // const [isFormValid, setIsFormValid] = useState(false);
-  const { data: profileDate, excute: fetchProfile } =
-    useAsync(createProfileImage);
+  const [values, setValues] = useState(DEFAULT_VALUES);
+  const [validations, setValidations] = useState(DEFAULT_PROFILE_VALIDATIONS);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const {
+    data: profileDate,
+    excute: _createProfileImage,
+    clear: claerProfile,
+  } = useAsync(createProfileImage);
   const { data: updateData, excute: _updateUser } = useAsync(updateUser);
 
+  /**
+   * 프로필 폼 입력 필드 값 변경 핸들러
+   * - 입력값 상태 관리
+   */
   const handleChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setValues((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setValues((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   };
 
+  /**
+   * 프로필 이미지 URL 생성 요청 혹은 제거 핸들러
+   * @param value
+   */
   const handleChangeProfile = (value: FormData | null) => {
     if (value) {
-      fetchProfile({ image: value });
+      _createProfileImage({ image: value });
     } else {
-      setValues((prev) => ({
-        ...prev,
-        profileImageUrl: null,
-      }));
+      claerProfile();
     }
   };
 
+  useEffect(() => {
+    setValidations((prev) => ({
+      ...prev,
+      nickname: validate.nickname(values.nickname),
+    }));
+  }, [values.nickname]);
+
+  useEffect(() => {
+    setValidations((prev) => ({
+      ...prev,
+      profileImageUrl: validate.profileImageUrl(values.profileImageUrl),
+    }));
+  }, [values.profileImageUrl]);
+
+  /**
+   * 유저 프로필 변경 요청
+   */
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     _updateUser({
@@ -54,17 +78,43 @@ export default function ProfileForm({
     });
   };
 
+  /**
+   * 폼 유효성 검사. 아래 조건을 모두 만족시키면 유효함
+   * - 프로필 URL, 닉네임 유효함
+   * - 프로필 URL과 닉네임 중 하나 이상 변경
+   */
   useEffect(() => {
+    const isValid =
+      validations.nickname.isValid && validations.profileImageUrl.isValid;
+    const isChanged =
+      profileImageUrl !== values.profileImageUrl ||
+      nickname !== values.nickname;
+    setIsFormValid(isValid && isChanged);
+  }, [validations, nickname, profileImageUrl, values]);
+
+  /**
+   * 프로필 이미지 생성 요청 성공 시
+   * - 프로필 URL 상태 관리
+   * - 프로필 URL 유효성 검사
+   */
+  useEffect(() => {
+    const next = profileDate?.profileImageUrl as string;
     setValues((prev) => ({
       ...prev,
-      profileImageUrl: profileDate?.profileImageUrl as string,
+      profileImageUrl: next,
     }));
   }, [profileDate]);
 
   useEffect(() => {
+    /**
+     * @todo useAuth에 프로필 업데이트 메서드 구현 후 적용
+     */
     console.log(updateData);
   }, [updateData]);
 
+  /**
+   * 프로필 정보 업데이트 시 반영
+   */
   useEffect(() => {
     setValues({ email, nickname, profileImageUrl });
   }, [email, nickname, profileImageUrl]);
@@ -82,7 +132,7 @@ export default function ProfileForm({
           profileImageUrl={values.profileImageUrl}
           onChange={handleChangeProfile}
         />
-        <div className="mt-10 flex flex-col gap-2 tablet:ml-10 tablet:mt-0 tablet:grow">
+        <div className="mt-10 flex flex-col gap-4 tablet:ml-10 tablet:mt-0 tablet:grow">
           <InputField
             name="email"
             type="text"
@@ -97,12 +147,13 @@ export default function ProfileForm({
             type="text"
             placeholder={nickname}
             value={values.nickname}
-            validation={{ isValid: false, message: '' }}
+            validation={validations.nickname}
             onChange={handleChangeValue}
           />
           <button
             className="w-full select-none rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
             type="submit"
+            disabled={!isFormValid}
           >
             저장
           </button>
