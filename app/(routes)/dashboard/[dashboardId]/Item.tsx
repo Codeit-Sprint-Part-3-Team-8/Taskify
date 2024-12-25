17c783950851bb314699c20c7802f4c358269a92
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-12-26 00:52:57 +0900

    feat: 대쉬보드 컨테이너 반응형 레이아웃 구현
    - Mobile: 컨테이너 1열, 카드 1행 횡 스크롤
    - Tablet: 컨테이너 1열, 카드 1열 종 스크롤
    - PC: 컨테이너 1행, 카드 1열 모든 카드(무한 스크롤 예정)

--- app/(routes)/dashboard/[dashboardId]/Item.tsx
+++ app/(routes)/dashboard/[dashboardId]/Item.tsx
@@ -8,7 +8,7 @@ interface ItemsProps {
 export function Item({ id, dragOverlay }: ItemsProps) {
   return (
     <div
-      className={`item ${dragOverlay ? 'cursor-grabbing' : 'cursor-grab'} rounded border-2 border-gray-200`}
+      className={`item h-[70px] w-[200px] mobile:w-full ${dragOverlay ? 'cursor-grabbing' : 'cursor-grab'} rounded border-2 border-gray-200 bg-gray-100 p-4`}
     >
       할 일 카드 {id}
     </div>
