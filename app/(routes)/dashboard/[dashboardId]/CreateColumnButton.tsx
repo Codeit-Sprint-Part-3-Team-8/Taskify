57595b2a903db47169135fcafd641ae00417fa8f
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-12-26 22:36:36 +0900

    feat: CreateCardButton 추가
    - 메소드는 아직 구현되지 않음

--- app/(routes)/dashboard/[dashboardId]/CreateColumnButton.tsx
+++ app/(routes)/dashboard/[dashboardId]/CreateColumnButton.tsx
@@ -4,7 +4,9 @@ export default function CreateColumnButton() {
   return (
     <div className="w-full px-3 mobile:pb-8 mobile:pt-4 pc:pt-[58px]">
       <div className="flex h-[70px] w-full items-center justify-center gap-3 rounded-md border px-24">
-        <p className="text-nowrap text-2lg font-bold">새로운 컬럼 추가하기</p>
+        <p className="text-nowrap text-lg font-bold tablet:text-2lg">
+          새로운 컬럼 추가하기
+        </p>
         <Image
           src="/images/icon/ic-plusbtn-purple.svg"
           alt="카드 생성 버튼"
