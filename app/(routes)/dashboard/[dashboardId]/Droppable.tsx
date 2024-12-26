979953dafb11bf52b8d617ae6d108cf41cef74ef
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-12-26 22:43:30 +0900

    feat: column 수정 버튼 추가

--- app/(routes)/dashboard/[dashboardId]/Droppable.tsx
+++ app/(routes)/dashboard/[dashboardId]/Droppable.tsx
@@ -4,6 +4,7 @@ import { rectSortingStrategy, SortableContext } from '@dnd-kit/sortable';
 import SortableItem from './SortableItem';
 import { CardType } from '@/_types/cards.type';
 import CreateCardButton from './CreateCardButton';
+import Image from 'next/image';
 
 interface DroppableProps {
   id: string;
@@ -24,18 +25,28 @@ export default function Droppable({
     <SortableContext id={id} items={items} strategy={rectSortingStrategy}>
       <div className="px-3 py-4 mobile:w-full pc:max-w-[384px]">
         <div className="border-b-2 pb-2 tablet:pb-6 pc:border-none">
-          <div className="mb-4 flex items-center gap-2">
-            <span
-              className="sh h-2 w-2 shrink-0 rounded-full"
-              style={{ backgroundColor: dashBoardColor }}
-            />
-            <div className="flex items-center gap-4">
-              <p className="text-nowrap text-lg font-bold tablet:text-2lg">
-                {title}
-              </p>
-              <p className="flex h-5 w-5 items-center justify-center rounded-md bg-gray-EEEEEE px-1.5 py-1 text-xs text-gray-787486">
-                {items.length}
-              </p>
+          <div className="flex justify-between pb-1">
+            <div className="mb-4 flex items-center gap-2">
+              <span
+                className="sh h-2 w-2 shrink-0 rounded-full"
+                style={{ backgroundColor: dashBoardColor }}
+              />
+              <div className="flex items-center gap-4">
+                <p className="text-nowrap text-lg font-bold tablet:text-2lg">
+                  {title}
+                </p>
+                <p className="flex h-5 w-5 items-center justify-center rounded-md bg-gray-EEEEEE px-1.5 py-1 text-xs text-gray-787486">
+                  {items.length}
+                </p>
+              </div>
+            </div>
+            <div className="relative h-5 w-5 tablet:h-[24px] tablet:w-[24px]">
+              <Image
+                src="/images/icon/ic-setting.svg"
+                alt="컬럼 수정 버튼"
+                layout="fill"
+                objectFit="contain"
+              />
             </div>
           </div>
           <CreateCardButton />
