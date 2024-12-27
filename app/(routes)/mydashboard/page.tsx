b3bde51efed0d75c2e3adc1e148dd5f4a1163de7
bhoh032019 <162112399+bhoh032019@users.noreply.github.com>
2024-12-27 15:04:59 +0900

    [Refactor] 나의 대시보드 리팩토링 (#133)
    * refactor: 대시보드 리스트 상위페이지에서 관리 후 props로 이용해 초대 수락 반영
    
    * refactor: DashboardType '/_types'폴더에서 이용하도록 수정
    
    * fix: 대시보드 제목에 따른 원 깨짐 현상 수정

--- app/(routes)/mydashboard/page.tsx
+++ app/(routes)/mydashboard/page.tsx
@@ -1,16 +1,64 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import NavBar from '@/_components/Navbar/NavBar';
 import Sidebar from '@/_components/Sidebar/SideBar';
 import DashboardList from './DashboardList';
-import NavBar from '@/_components/Navbar/NavBar';
 import InvitationsDashboard from './InvitationsDashboard';
+import useAsync from '@/_hooks/useAsync';
+import { getDashboardList } from '@/api/dashboards.api';
+import CreateDashboardModal from './CreateDashboardModal';
+import { DashboardType } from '@/_types/dashboards.type';
 
+export default function MyDashboardPage() {
+  const [dashboardList, setDashboardList] = useState<DashboardType[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-export default function myDashBoardPage() {
+  const { excute: fetchDashboards } = useAsync(async (page: number) => {
+    const data = await getDashboardList({
+      navigationMethod: 'pagination',
+      page,
+      size: 5,
+    });
+    setDashboardList(data.dashboards);
+    setTotalPages(Math.ceil(data.totalCount / 5));
+  });
+
+  useEffect(() => {
+    fetchDashboards(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handleOpenCreateModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseCreateModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleAcceptInvitation = () => {
+    fetchDashboards(currentPage);
+  };
+
   return (
     <div>
       <NavBar type="mydashboard" />
       <Sidebar />
-      <DashboardList />
-      <InvitationsDashboard />
+      <DashboardList
+        dashboardList={dashboardList}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+        onOpenCreateModal={handleOpenCreateModal}
+      />
+      <InvitationsDashboard onAcceptInvitation={handleAcceptInvitation} />
+      {isModalOpen && <CreateDashboardModal onClose={handleCloseCreateModal} />}
     </div>
   );
 }
