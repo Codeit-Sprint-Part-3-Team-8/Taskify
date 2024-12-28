c733a2eeaa7949f93056d64d0d0f2c026fb63350
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-28 13:42:20 +0900

    refactor: 불필요한 useEffect, useState 제거

--- app/(routes)/mydashboard/DashboardList.tsx
+++ app/(routes)/mydashboard/DashboardList.tsx
@@ -3,7 +3,6 @@
 import { DashboardType } from '@/_types/dashboards.type';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 
 interface DashboardListProps {
   dashboardList: DashboardType[];
