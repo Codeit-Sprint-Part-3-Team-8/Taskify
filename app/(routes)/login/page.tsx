cfce36fdb34d571348d132d5c62e288c2c12503b
WooLegend <dnwogus4260@naver.com>
2024-12-28 21:41:11 +0900

    fix: 문제 해결

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -3,13 +3,12 @@
 import AuthFooter from '@/_components/Auth/AuthFooter';
 import AuthHeader from '@/_components/Auth/AuthHeader';
 import InputField from '@/_components/Auth/InputField';
-import { useCallback, useEffect, useState } from 'react';
+import { Suspense, useCallback, useEffect, useState } from 'react';
 import { ValuesType } from './loginType';
 import { DEFAULT_VALIDATIONS, validateSchema } from './validate';
 import { useAuth } from '@/context/AuthContext';
 import Modal from '@/_components/Auth/Modal';
 import { useRouter, useSearchParams } from 'next/navigation';
-
 const DEFAULT_VALUES: ValuesType = {
   email: '',
   password: '',
@@ -21,8 +20,6 @@ export default function LoginPage() {
   const [isFormValid, setIsFormValid] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const { user, login, loading, errorMessage } = useAuth();
-  const router = useRouter();
-  const goto = useSearchParams().get('goto') || 'mydashboard';
 
   const handleCloseModal = () => setShowModal(false);
 
@@ -63,44 +60,61 @@ export default function LoginPage() {
     validateForm();
   }, [validateForm]);
 
-  if (user) {
-    router.push(goto);
-    return null;
-  }
-
   return (
-    <>
-      {showModal && (
-        <Modal text={errorMessage.login as string} onClick={handleCloseModal} />
+    <Suspense fallback={<div>loading</div>}>
+      {user ? (
+        <Navigate />
+      ) : (
+        <>
+          {showModal && (
+            <Modal
+              text={errorMessage.login as string}
+              onClick={handleCloseModal}
+            />
+          )}
+          <div className="mx-auto w-full max-w-xs pb-8 pt-20 tablet:max-w-lg tablet:pt-52">
+            <AuthHeader text="오늘도 만나서 반가워요!" />
+            <form className="mb-6 flex flex-col gap-6" onSubmit={handleSubmit}>
+              <InputField
+                name="email"
+                value={values.email}
+                validation={validations.email}
+                onChange={handleChangeValue}
+                onBlur={handleBlurInput}
+              />
+              <InputField
+                name="password"
+                type="password"
+                value={values.password}
+                validation={validations.password}
+                onChange={handleChangeValue}
+                onBlur={handleBlurInput}
+              />
+              <button
+                className="w-full select-none rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
+                type="submit"
+                disabled={!isFormValid || loading.login}
+              >
+                로그인
+              </button>
+            </form>
+            <AuthFooter to="signup" />
+          </div>
+        </>
       )}
-      <div className="mx-auto w-full max-w-xs pb-8 pt-20 tablet:max-w-lg tablet:pt-52">
-        <AuthHeader text="오늘도 만나서 반가워요!" />
-        <form className="mb-6 flex flex-col gap-6" onSubmit={handleSubmit}>
-          <InputField
-            name="email"
-            value={values.email}
-            validation={validations.email}
-            onChange={handleChangeValue}
-            onBlur={handleBlurInput}
-          />
-          <InputField
-            name="password"
-            type="password"
-            value={values.password}
-            validation={validations.password}
-            onChange={handleChangeValue}
-            onBlur={handleBlurInput}
-          />
-          <button
-            className="w-full select-none rounded-lg border bg-violet-5534DA py-3.5 text-lg font-medium text-white disabled:bg-gray-9FA6B2"
-            type="submit"
-            disabled={!isFormValid || loading.login}
-          >
-            로그인
-          </button>
-        </form>
-        <AuthFooter to="signup" />
-      </div>
-    </>
+    </Suspense>
   );
+}
+
+function Navigate() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const goto = searchParams.get('goto') || 'mydashboard';
+
+  useEffect(() => {
+    if (typeof window !== undefined && searchParams) {
+      router.push(goto);
+    }
+  });
+  return null;
 }
