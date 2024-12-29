1a6d4125c02de5f8e85fe2a92976c3e29b71204b
k-jipgeria <sgim43547@gmail.com>
2024-12-29 14:55:35 +0900

    feat: 댓글 관련 기능을 모아둔 훅 추가

---
+++ app/_hooks/useCommentList.ts
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { CommentListType } from '@/_types/comments.type';
+import {
+  createComment,
+  getCommentList,
+  deleteComment,
+  updateComment,
+} from '@/api/comments.api';
+
+export default function useCommentList(
+  cardId: number,
+  initialComments: CommentListType | null,
+) {
+  const [commentsResponse, setCommentsResponse] =
+    useState<CommentListType | null>(initialComments);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // 댓글 추가
+  const addComment = async (
+    content: string,
+    dashboardId: number,
+    columnId: number,
+  ) => {
+    if (!content.trim()) return;
+
+    setIsSubmitting(true);
+    try {
+      await createComment({
+        content,
+        cardId,
+        columnId,
+        dashboardId,
+      });
+      // setCommentsResponse((prev) => ({
+      //   ...prev,
+      //   comments: [addedComment, ...(prev?.comments || [])],
+      // }));
+    } catch (error) {
+      console.error('댓글 추가 에러:', error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  // 댓글 더 불러오기
+  const loadMoreComments = async (cursorId?: number) => {
+    setIsSubmitting(true);
+    try {
+      const newCommentsResponse = await getCommentList({ cardId, cursorId });
+      setCommentsResponse((prev) => ({
+        ...newCommentsResponse,
+        comments: [...(prev?.comments || []), ...newCommentsResponse.comments],
+      }));
+    } catch (error) {
+      console.error('댓글을 불러오는데 실패했습니다:', error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  // 댓글 삭제
+  const removeComment = async (commentId: number) => {
+    try {
+      await deleteComment({ commentId });
+      // setCommentsResponse((prev) => ({
+      //   ...prev,
+      //   comments:
+      //     prev?.comments.filter((comment) => comment.id !== commentId) || [],
+      // }));
+    } catch (error) {
+      console.error('댓글 삭제 에러:', error);
+    }
+  };
+
+  // 댓글 수정
+  const editComment = async (commentId: number, newContent: string) => {
+    try {
+      const updatedComment = await updateComment({
+        commentId,
+        content: newContent,
+      });
+
+      setCommentsResponse((prev) =>
+        prev
+          ? {
+              ...prev,
+              comments: prev.comments.map((comment) =>
+                comment.id === commentId
+                  ? { ...comment, content: updatedComment.content }
+                  : comment,
+              ),
+            }
+          : null,
+      );
+    } catch (error) {
+      console.error('댓글 수정 에러:', error);
+    }
+  };
+
+  return {
+    commentsResponse,
+    addComment,
+    loadMoreComments,
+    removeComment,
+    editComment,
+    isSubmitting,
+  };
+}
