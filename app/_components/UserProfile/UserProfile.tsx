4d340b574f1997f6881aa1455a4ddc5054b605f9
k-jipgeria <sgim43547@gmail.com>
2024-12-29 14:52:47 +0900

    feat: 할 일 카드 댓글에서 보일 프로필 컴포넌트 구현

---
+++ app/_components/UserProfile/UserProfile.tsx
@@ -0,0 +1,29 @@
+import * as Avatar from '@radix-ui/react-avatar';
+
+interface UserProfileProps {
+  profileImageUrl?: string | null;
+  onlyImg?: boolean;
+  nickname: string;
+}
+
+export default function UserProfile({
+  profileImageUrl,
+  onlyImg,
+  nickname,
+}: UserProfileProps) {
+  return (
+    <div className="flex items-center">
+      <Avatar.Root className="inline-flex select-none items-center justify-center overflow-hidden rounded-full bg-gray-200 align-middle">
+        <Avatar.Image
+          className="h-[1.625rem] w-[1.625rem] object-cover tablet:h-[2.375rem] tablet:w-[2.375rem]"
+          src={profileImageUrl || ''}
+          alt={nickname}
+        />
+        <Avatar.Fallback className="flex h-[1.625rem] w-[1.625rem] items-center justify-center bg-gray-200 font-semibold text-gray-600 tablet:h-[2.375rem] tablet:w-[2.375rem]">
+          {nickname[0].toUpperCase()}
+        </Avatar.Fallback>
+      </Avatar.Root>
+      {!onlyImg && <span className="ml-2 text-sm font-medium">{nickname}</span>}
+    </div>
+  );
+}
