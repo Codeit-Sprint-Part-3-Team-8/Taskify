72c9663808a91853edcadbf563cc7ab81321304f
WooLegend <dnwogus4260@naver.com>
2024-12-31 02:47:44 +0900

    feat: 태그 말 줄임 구현

--- app/(routes)/dashboard/[dashboardId]/_TodoCardModal/ModalContent.tsx
+++ app/(routes)/dashboard/[dashboardId]/_TodoCardModal/ModalContent.tsx
@@ -10,7 +10,7 @@ interface ModalContentProps {
 export default function ModalContent({ card, columnTitle }: ModalContentProps) {
   return (
     <div className="flex flex-col gap-4 tablet:flex-row-reverse">
-      <div className="flex h-fit items-center justify-between gap-4 rounded-xl border border-gray-D9D9D9 px-4 py-[0.375rem] tablet:w-40 tablet:flex-col tablet:items-start tablet:px-4 tablet:py-3">
+      <div className="flex h-fit items-center justify-between gap-4 rounded-xl border border-gray-D9D9D9 px-4 py-[0.375rem] tablet:w-48 tablet:flex-col tablet:items-start tablet:px-4 tablet:py-3">
         <div className="flex h-[2.875rem] grow flex-col justify-between">
           <label className="text-black-323236 text-xs font-semibold">
             담당자
@@ -35,23 +35,21 @@ export default function ModalContent({ card, columnTitle }: ModalContentProps) {
       </div>
 
       <div className="grow">
-        <div className="mb-4 flex h-[26px] w-full items-center gap-2">
-          <div className="flex h-full max-w-40 items-center gap-1 rounded-full bg-violet-8 pr-2">
+        <div className="mb-4 flex flex-wrap items-center gap-2">
+          <div className="flex h-[26px] max-w-40 items-center gap-1 rounded-full bg-violet-8 pr-2">
             <span className="ml-[6px] text-3xl text-violet-5534DA">•</span>
             <span className="mt-1 truncate whitespace-nowrap text-xs text-violet-5534DA">
               {columnTitle}
             </span>
           </div>
-          <div className="flex h-full gap-2 truncate">
-            {card?.tags.map((tag) => (
-              <span
-                key={tag}
-                className="h-full whitespace-nowrap rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-600"
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
+          {card?.tags.map((tag) => (
+            <span
+              key={tag}
+              className="h-[26px] max-w-20 truncate whitespace-nowrap rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-600"
+            >
+              {tag}
+            </span>
+          ))}
         </div>
 
         <div className="mb-6 h-[272px]">
