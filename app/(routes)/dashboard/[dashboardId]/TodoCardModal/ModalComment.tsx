5fa4aafb3a3c5e6acf9cf59985b7b096bd267067
WooLegend <dnwogus4260@naver.com>
2024-12-31 01:51:54 +0900

    feat: 댓글 컨텐츠 표기

--- app/(routes)/dashboard/[dashboardId]/TodoCardModal/ModalComment.tsx
+++ app/(routes)/dashboard/[dashboardId]/TodoCardModal/ModalComment.tsx
@@ -111,6 +111,7 @@ export default function ModalComment({
                   </div>
                 )} */}
               </div>
+              <div>{comment.content}</div>
             </div>
           ))}
         </div>
