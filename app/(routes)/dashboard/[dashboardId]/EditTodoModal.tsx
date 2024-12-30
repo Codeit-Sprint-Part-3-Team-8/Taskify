da6072baab9afd001c33c7decd68291395060b2e
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-12-30 20:59:59 +0900

    대쉬보드 할 일 카드 생성/삭제/수정 시 상태 업데이트 (#192)
    * feat: 할 일 카드 생성 시 상태 업데이트
    
    * feat: 할 일 카드 수정 시 상태 업데이트
    
    * feat: 할 일 카드 모달 현재 컬럼 태그 수정
    
    - 컬럼이 아니라 카드의 title이 들어가고 있던 오류 수정
    
    * feat: 할 일 카드 삭제 시 상태 업데이트

--- app/(routes)/dashboard/[dashboardId]/EditTodoModal.tsx
+++ app/(routes)/dashboard/[dashboardId]/EditTodoModal.tsx
@@ -11,6 +11,7 @@ interface EditTodoModalProps {
   columnTitle: string;
   card: CardType;
   columns: Array<{ columnId: number; columnTitle: string }>;
+  onEditCard: (columnId: number, updatedCard: CardType) => void;
   members: MemberType[];
   onClose: () => void;
 }
@@ -20,6 +21,7 @@ export default function EditTodoModal({
   card,
   columns,
   members,
+  onEditCard,
   onClose,
 }: EditTodoModalProps) {
   const [formData, setFormData] = useState({
@@ -47,10 +49,11 @@ export default function EditTodoModal({
   const handleEditTodo = async () => {
     setIsLoading(true);
     try {
-      await updateCard({
+      const response = await updateCard({
         cardId: card.id,
         ...formData,
       });
+      onEditCard(formData.columnId, response);
       onClose();
     } catch (error) {
       alert('할 일 수정에 실패했습니다.');
