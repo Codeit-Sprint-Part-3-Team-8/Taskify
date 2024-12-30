d6e40cf9bd2c4297780d64d0a3bfd7ac149ac8ac
WooLegend <dnwogus4260@naver.com>
2024-12-30 19:24:40 +0900

    fix: ModalInput 컴포넌트 outline 컬러 적용

--- app/_components/Modals/ModalInput.tsx
+++ app/_components/Modals/ModalInput.tsx
@@ -1,7 +1,7 @@
 const FIELD_STYLES = 'w-full flex flex-col gap-2';
 const FIELD_LABEL_STYLES = 'text-lg font-medium text-black-333236 select-none';
 const FIELD_INPUT_STYLES =
-  'h-12 rounded-xl border border-gray-D9D9D9 px-3 placeholder:select-none placeholder:text-gray-9FA6B2';
+  'h-12 rounded-xl border border-gray-D9D9D9 px-3 placeholder:select-none placeholder:text-gray-9FA6B2 focus-visible:outline-violet-5534DA';
 
 const MODAL_INPUT_TEXT: {
   [key: string]: {
