e43e32c9b27854dbeaac96fbd4b85856e9dd7be7
WooLegend <dnwogus4260@naver.com>
2024-12-31 05:21:18 +0900

    fix: 할 일 모달 레이아웃 수정

--- app/(routes)/dashboard/[dashboardId]/_TodoCardModal/ModalContent.tsx
+++ app/(routes)/dashboard/[dashboardId]/_TodoCardModal/ModalContent.tsx
@@ -7,7 +7,7 @@ interface ModalContentProps {
 
 export default function ModalContent({ card }: ModalContentProps) {
   return (
-    <div className="overtext-xs break-words font-normal text-black-000000">
+    <div className="overtext-xs mb-4 break-words font-normal text-black-000000">
       {card?.description}
       {card?.imageUrl && (
         <div className="relative mb-4 mt-4 h-[10.5rem] w-[18.125rem] rounded-lg tablet:h-[15.375rem] tablet:w-[26.25rem] pc:h-[16.25rem] pc:w-[27.81375rem]">
